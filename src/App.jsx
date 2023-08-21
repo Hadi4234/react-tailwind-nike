d3f9724a66ef208b1c6c303fc076539c06412203
Hadi4234 <48425016+Hadi4234@users.noreply.github.com>
2023-08-21 17:39:07 +0600

    just finish home page

--- src/App.jsx
+++ src/App.jsx
@@ -29,12 +29,6 @@ const App = () => {
       <section className='padding'>
         <SpecialOffer />
       </section>
-      <section className='bg-pale-blue padding'>
-        <CustomerReviews />
-      </section>
-      <section className='padding-x sm:py-32 py-16 w-full'>
-        <Subscribe />
-      </section>
       <section className=' bg-black padding-x padding-t pb-8'>
         <Footer />
       </section>
